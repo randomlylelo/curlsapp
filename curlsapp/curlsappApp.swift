8c275ab9ed9dc572a85034b6774c4f3f6e505d70
Leo <38267386+randomlylelo@users.noreply.github.com>
2025-08-01 11:50:22 -0400

    Initial Commit

---
+++ curlsapp/curlsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  curlsappApp.swift
+//  curlsapp
+//
+//  Created by Leo on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct curlsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ curlsappTests/curlsappTests.swift
@@ -0,0 +1,16 @@
+//
+//  curlsappTests.swift
+//  curlsappTests
+//
+//  Created by Leo on 8/1/25.
+//
+
+import Testing
+
+struct curlsappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
